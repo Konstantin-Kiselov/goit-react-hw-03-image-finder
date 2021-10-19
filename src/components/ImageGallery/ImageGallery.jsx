bc88de2ddf2a87f6b79b7ece6df366815f792af7
Konstantin-Kiselov <constantine.kiss@gmail.com>
2021-10-20 01:19:04 +0300

    try to fix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -10,28 +10,24 @@ class ImageGallery extends Component {
     status: 'idle',
   };
 
-  componentDidMount() {
-    const API_KEY = '23027480-c70d45ac3781d0e477b4a7117';
-    const url = `https://pixabay.com/api/?q=${this.props.searchQuery}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
-
-    fetch(url)
-      .then(res => res.json())
-      .then(gallery => {
-        // console.log(gallery);
-        this.setState({ gallery: gallery.hits });
-      });
-    // .finally(() => this.setState({ loading: false }));
-  }
-
   componentDidUpdate(prevProps) {
     if (prevProps.searchQuery !== this.props.searchQuery) {
+      this.setState({ status: 'pending', gallery: null });
+
       const API_KEY = '23027480-c70d45ac3781d0e477b4a7117';
       const url = `https://pixabay.com/api/?q=${this.props.searchQuery}&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`;
 
       fetch(url)
         .then(res => res.json())
-        .then(gallery => this.setState({ gallery: gallery.hits }))
-        .catch(error => this.setState({ error }));
+        .then(gallery => {
+          if (gallery.hits.length > 0) {
+            this.setState({ gallery: gallery.hits, status: 'resolved' });
+          } else {
+            alert(`По запросу ${this.props.searchQuery} ничего не найдено.`);
+            this.setState({ status: 'idle' });
+          }
+        })
+        .catch(error => this.setState({ error, status: 'rejected' }));
     }
   }
 
@@ -39,34 +35,35 @@ class ImageGallery extends Component {
     const { onClick } = this.props;
     const { gallery, error, status } = this.state;
 
-    // if (status === 'idle') {
-    //   return;
-    // }
+    if (status === 'idle') {
+      return <></>;
+    }
 
     if (status === 'pending') {
       return <div>Загружаем...</div>;
     }
 
-    return (
-      <>
-        {error && <h1>Нет результатов поиска</h1>}
-        {gallery && (
-          <ul className={s.imageGallery}>
-            {gallery.map(({ id, webformatURL, largeImageURL, tags }) => {
-              return (
-                <ImageGalleryItem
-                  key={id}
-                  src={webformatURL}
-                  dataSrc={largeImageURL}
-                  tags={tags}
-                  onClick={onClick}
-                />
-              );
-            })}
-          </ul>
-        )}
-      </>
-    );
+    if (status === 'rejected') {
+      return <h1>{error.message}</h1>;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <ul className={s.imageGallery}>
+          {gallery.map(({ id, webformatURL, largeImageURL, tags }) => {
+            return (
+              <ImageGalleryItem
+                key={id}
+                src={webformatURL}
+                dataSrc={largeImageURL}
+                tags={tags}
+                onClick={onClick}
+              />
+            );
+          })}
+        </ul>
+      );
+    }
   }
 }
 
